1dbb53745c5471d0fcd882242fe967da033cf507
hiroto <s266008@wakayama-u.ac.jp>
2025-04-21 19:44:32 +0900

    CN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 // app/page.tsx
 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <p>
+        <Link href="https://addmaru.tech" target="_blank" rel="noopener noreferrer">
+          add。のホームページです
+        </Link>
+      </p>
     </main>
   );
 }
