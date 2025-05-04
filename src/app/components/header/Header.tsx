9f71809df8145dea4eabf2fa554eb2faf5c0fd61
hiroto <s266008@wakayama-u.ac.jp>
2025-05-04 13:33:56 +0900

    GanttChartコンポーネントの場所の作成

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>Header
+     <Link href="a"></Link>
+    </div>
+  )
+}
+
+export default Header
